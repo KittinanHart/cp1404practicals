01c5207550e8ad1639a40e1ecb941d8d7f76b6b9
KittinanHart <kittinanhart18@gmail.com>
2023-09-24 21:19:01 +1000

    Add function to take in the user's score as a parameter and returns the result to be printed.

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,15 +3,22 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-# TODO: Fix this!
 
-score = float(input("Enter score: "))
+def main():
+    score = float(input("Enter score: "))
+    result = grade_score(score)
+    print(result)
 
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else: 
-    print("Bad")
+
+def grade_score(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
